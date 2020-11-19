fdefe32add990acccefc338cdbd8e7549ee01f66
Martincou <martincoutinho00@gmail.com>
2020-11-19 19:47:13 -0300

    added countdown to numbers

--- js/main.js
+++ js/main.js
@@ -16,9 +16,19 @@ $(function(){
     return false;
   })
 
-
+  //Numeros animados
   $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
   $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1500);
   $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1200);
   $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+  //Cuenta regresiva
+$('.cuenta-regresiva').countdown('2020/12/10 09:00:00', function(event){
+  $('#dias').html(event.strftime('%D'));
+  $('#horas').html(event.strftime('%H'));
+  $('#minutos').html(event.strftime('%M'));
+  $('#segundos').html(event.strftime('%S'));
+
+})
+
 })
